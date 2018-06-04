9122d111fca8fcb21ae0480ae23fddbf8b6820f5
atkinsta <atkinstaa@gmail.com>
2018-06-04 11:36:42 -0400

    Added the main functionality in JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,37 @@ var guessesLeft;
 var guessList = [];
 
 var targetWords = ["Taj Mahal", "Everest", "Mt Rushmore", "Yosemite"]
-var hiddenWord = targetWord.replace(/[^\s]/g, "_");
+var chosenWord = targetWords[Math.floor(Math.random() * targetWords.length)];
+var copiedWord = chosenWord
+var hiddenWord = chosenWord.replace(/[^\s]/g, "_");
 
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
 console.log(hiddenWord);
-console.log(targetWord);
+document.onkeydown = function(event) {
+    var userGuess = event.key;
+    var location = copiedWord.toLowerCase().indexOf(userGuess);
+    console.clear();
+    if (location == -1){
+
+        if (guessList.indexOf(userGuess) !== -1){
+            console.log("Already guessed");
+        }
+        else {
+            guessList.push(userGuess); 
+        }
+    }
+    else {  
+        while (location !== -1){
+        copiedWord = copiedWord.replaceAt(location, "0");
+        hiddenWord = hiddenWord.replaceAt(location, userGuess); 
+        location = copiedWord.toLowerCase().indexOf(userGuess);
+        }
+        if (hiddenWord == chosenWord.toLowerCase())
+            alert("You win!");
+    }
+    console.log(hiddenWord);
+    console.log(guessList);
+    console.log(copiedWord.indexOf(userGuess))
+}
