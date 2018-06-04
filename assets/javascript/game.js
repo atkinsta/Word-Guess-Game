6db201f1eedd0709490bf9b8540f6c44a9c73a0f
atkinsta <atkinstaa@gmail.com>
2018-06-04 09:11:01 -0400

    Started the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,9 @@
 var wins;
 var guessesLeft;
+var guessList = [];
+
+var targetWords = ["Taj Mahal", "Everest", "Mt Rushmore", "Yosemite"]
+var hiddenWord = targetWord.replace(/[^\s]/g, "_");
+
+console.log(hiddenWord);
+console.log(targetWord);
